7aeb2c068761658a95363037ec9476a5ea656f3b
vaishnavi2504 <vaishnavibhat26@gmail.com>
2016-01-18 13:11:35 -0600

    Update profanityCheck.py

--- profanityCheck.py
+++ profanityCheck.py
@@ -1,6 +1,7 @@
 import urllib.request
 import re
 
+#This function reads the input file from the file and replaces all newline characters with a space
 def read_text():
  quotes=open('C:\Python34\movie.txt')
  file_content=quotes.read()
@@ -9,6 +10,8 @@ def read_text():
  quotes.close()
  profanity_check(file_new_content)
  
+ #Profanity checking function which uses wdyl profanity checker and spits out true
+ # if the input file contains profanity words and false otherwise
 def profanity_check(text_to_check):
  file_new_content=re.sub(' +', '%20',text_to_check)
  response=urllib.request.urlopen("http://www.wdyl.com/profanity?q="+file_new_content)
